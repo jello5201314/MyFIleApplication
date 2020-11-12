6d8896297b4021d07e0dfd36ad01dc0b57731719
huangguodong <haungguodong@hengli.com>
2020-11-12 10:31:05 +0800

    88888888

--- app/src/main/java/com/example/myapplication/FolderActivity.java
+++ app/src/main/java/com/example/myapplication/FolderActivity.java
@@ -41,7 +41,7 @@ public class FolderActivity extends Activity implements OnItemClickListener,OnCl
     baseFile=GetFilesUtils.getInstance().getBasePath(); 
 
 
-//    titleTv=(TextView) findViewById(R.id.mtitle);    66666666666
+//    titleTv=(TextView) findViewById(R.id.mtitle);    8888888888888
 //    titleTv.setText("本地文件");
     verifyStoragePermissions(this);
     folderLv=(ListView) findViewById(R.id.folder_list); 
